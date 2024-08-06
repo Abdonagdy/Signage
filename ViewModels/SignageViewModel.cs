d4a4f0d1ea40bbfffba93545519adba0135023f0
Abdelhamid Mahmoud <44057221+Abdonagdy@users.noreply.github.com>
2024-08-06 08:24:14 +0300

    Initial commit

---
+++ ViewModels/SignageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MizeSignage.ViewModels
+{
+    public class SignageViewModel
+    {
+        public int Id { get; set; } // int
+        public string SignageUrl { get; set; } // nvarchar(max)
+        public int? Duration { get; set; } // int
+        public bool? IsActive { get; set; } // bit
+        public int? DisplayOrder { get; set; } // int
+    }
+}
